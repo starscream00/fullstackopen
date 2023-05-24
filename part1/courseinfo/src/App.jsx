6f25f1a00dc9e3da24765a6785fda0b6dafe90b9
starscream00 <razafimamonjyrijahasina@gmail.com>
2023-05-24 15:07:04 +0300

    1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,10 +4,20 @@ const Header = (props) => {
 };
 const Content = (props) => {
   return (
-    <p>
-      {" "}
-      {props.part} {props.exercises}
-    </p>
+    <div>
+      <Part
+        part={props.part1}
+        exercises={props.exercises1}
+      />
+      <Part
+        part={props.part2}
+        exercises={props.exercises2}
+      />
+      <Part
+        part={props.part3}
+        exercises={props.exercises3}
+      />
+    </div>
   );
 };
 const Total = (props) => {
@@ -15,6 +25,13 @@ const Total = (props) => {
     <p>
       Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
     </p>
   );
 };
@@ -31,16 +48,12 @@ const App = () => {
     <div>
       <Header course={course} />
       <Content
-        part={part1}
-        exercise={exercises1}
-      />
-      <Content
-        part={part2}
-        exercise={exercises2}
-      />
-      <Content
-        part={part3}
-        exercise={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
       />
       <Total
         exercises1={exercises1}
