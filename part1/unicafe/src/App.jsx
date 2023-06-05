d012c9a91b52652f31032a9a3d09b6dd46b1ef37
starscream00 <razafimamonjyrijahasina@gmail.com>
2023-06-05 08:01:42 +0300

    1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,43 +11,78 @@ const Stats = (props) => {
     </p>
   );
 };
+
 const App = () => {
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
 
-  const clickHandler = (setter, newValue) => {
-    setter(newValue + 1);
+  const [stats, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const clickHandler = (name, stats) => {
+    setStats({
+      ...stats,
+      [name]: stats[name] + 1,
+    });
   };
   return (
     <>
       <h2>Give Feedbacks</h2>
+
       <Button
         label={"good"}
-        handleClick={() => clickHandler(setGood, good)}
+        handleClick={() => clickHandler("good", stats)}
       />
       <Button
         label={"neutral"}
-        handleClick={() => clickHandler(setNeutral, neutral)}
+        handleClick={() => clickHandler("neutral", stats)}
       />
       <Button
         label={"bad"}
-        handleClick={() => clickHandler(setBad, bad)}
+        handleClick={() => clickHandler("bad", stats)}
       />
 
       <h2>Statistics</h2>
       <Stats
         label={"good"}
-        value={good}
+        value={stats.good}
       />
       <Stats
         label={"neutral"}
-        value={neutral}
+        value={stats.neutral}
       />
       <Stats
         label={"bad"}
-        value={bad}
+        value={stats.bad}
+      />
+      <Stats
+        label={"all"}
+        value={stats.good + stats.neutral + stats.bad}
+      />
+
+      <Stats
+        label={"average"}
+        value={
+          stats.good + stats.neutral + stats.bad
+            ? (stats.good - stats.bad) /
+              (stats.good + stats.neutral + stats.bad)
+            : 0
+        }
+      />
+      <Stats
+        label={"positive"}
+        value={
+          (stats.good
+            ? stats.good / (stats.good + stats.neutral + stats.bad)
+            : 0) *
+            100 +
+          " %"
+        }
       />
     </>
   );
