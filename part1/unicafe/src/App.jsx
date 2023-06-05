4f6f8d5d1fb98f3db39d18ea351ffb31a6c2b755
starscream00 <razafimamonjyrijahasina@gmail.com>
2023-06-05 08:10:52 +0300

    part 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,41 +12,9 @@ const Stats = (props) => {
   );
 };
 
-const App = () => {
-  // save clicks of each button to its own state
-  // const [good, setGood] = useState(0);
-  // const [neutral, setNeutral] = useState(0);
-  // const [bad, setBad] = useState(0);
-
-  const [stats, setStats] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
-
-  const clickHandler = (name, stats) => {
-    setStats({
-      ...stats,
-      [name]: stats[name] + 1,
-    });
-  };
+const Statistics = ({ stats }) => {
   return (
     <>
-      <h2>Give Feedbacks</h2>
-
-      <Button
-        label={"good"}
-        handleClick={() => clickHandler("good", stats)}
-      />
-      <Button
-        label={"neutral"}
-        handleClick={() => clickHandler("neutral", stats)}
-      />
-      <Button
-        label={"bad"}
-        handleClick={() => clickHandler("bad", stats)}
-      />
-
       <h2>Statistics</h2>
       <Stats
         label={"good"}
@@ -84,6 +52,45 @@ const App = () => {
           " %"
         }
       />
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  // const [good, setGood] = useState(0);
+  // const [neutral, setNeutral] = useState(0);
+  // const [bad, setBad] = useState(0);
+
+  const [stats, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const clickHandler = (name, stats) => {
+    setStats({
+      ...stats,
+      [name]: stats[name] + 1,
+    });
+  };
+  return (
+    <>
+      <h2>Give Feedbacks</h2>
+
+      <Button
+        label={"good"}
+        handleClick={() => clickHandler("good", stats)}
+      />
+      <Button
+        label={"neutral"}
+        handleClick={() => clickHandler("neutral", stats)}
+      />
+      <Button
+        label={"bad"}
+        handleClick={() => clickHandler("bad", stats)}
+      />
+      <Statistics stats={stats} />
     </>
   );
 };
