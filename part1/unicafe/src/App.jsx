ae873b4398f89d3a811c5c9bb17574b99f9cc3d5
starscream00 <razafimamonjyrijahasina@gmail.com>
2023-06-05 09:00:11 +0300

    part 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -65,11 +65,6 @@ const Statistics = ({ stats }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
-  // const [good, setGood] = useState(0);
-  // const [neutral, setNeutral] = useState(0);
-  // const [bad, setBad] = useState(0);
-
   const [stats, setStats] = useState({
     good: 0,
     neutral: 0,
