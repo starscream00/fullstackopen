24722824bca2067962873b16fa8ea93dbfa08310
starscream00 <razafimamonjyrijahasina@gmail.com>
2023-06-13 16:33:41 +0300

    part 1.14

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const Button = ({ clickHandler, text }) => {
+  return <button onClick={clickHandler}>{text}</button>;
+};
+const Top = ({ votes, anecdotes }) => {
+  let maxIndex = votes.reduce((max, x, i, arr) => (x > arr[max] ? i : max), 0);
+  return (
+    <>
+      <h2>Anectode with most votes</h2>
+      <p>{anecdotes[maxIndex]}</p>
+      <p>has {votes[maxIndex]} votes</p>
+    </>
+  );
+};
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(
+    Math.floor(Math.random() * anecdotes.length)
+  );
+  const [votes, setvotes] = useState(new Array(anecdotes.length).fill(0));
+  const clickHandler = () => {
+    const random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  };
+
+  const vote = () => {
+    const tmp = [...votes];
+    tmp[selected] += 1;
+
+    setvotes(tmp);
+  };
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <p>
+        {anecdotes[selected]} pos {selected}
+      </p>
+      <p>has {votes[selected]} votes</p>
+      <br />
+      <Button
+        clickHandler={vote}
+        text={"vote"}
+      />
+      <Button
+        clickHandler={clickHandler}
+        text={"next anecdote"}
+      />
+
+      <Top
+        votes={votes}
+        anecdotes={anecdotes}
+      />
+    </div>
+  );
+};
+
+export default App;
